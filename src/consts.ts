0c3ea5181a08755c78591e406f5430b0f0853548
renhao.szu <renhao.szu@gmail.com>
2025-08-01 06:34:25 +0000

    change index

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'p1gd0g\'s blog';
+export const SITE_DESCRIPTION = '投资 旅游 编程 健身 一万年太短，不争朝夕';
