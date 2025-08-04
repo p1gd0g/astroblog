b668b1deaa7d4523a902595e9ae07ed7c9ca0e57
renhao.szu <renhao.szu@gmail.com>
2025-08-04 06:38:38 +0000

    fix sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://www.p1gd0g.com',
   integrations: [mdx(), sitemap()],
 
   vite: {
