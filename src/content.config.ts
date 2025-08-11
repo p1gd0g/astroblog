25e32b0af5689d20dd87e37a2bf7671ecc6029bb
renhao.szu <renhao.szu@gmail.com>
2025-08-11 06:53:36 +0000

    fix updated date

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,7 +10,7 @@ const blog = defineCollection({
 		description: z.string().optional(),
 		// Transform string to Date object
 		pubDate: z.string().transform((x) => {
-			
+
 			// parse x with format '2025 年 5 月 14 日 16:33:52' to Date
 			const parts = x.match(/(\d+) 年 (\d+) 月 (\d+) 日 (\d+):(\d+):(\d+)/);
 			if (!parts) return new Date();
@@ -23,7 +23,20 @@ const blog = defineCollection({
 			return new Date(year, month, day, hour, minute, second);
 		}
 		),
-		updatedDate: z.coerce.date().optional(),
+		updatedDate: z.string().transform((x) => {
+
+			// parse x with format '2025 年 5 月 14 日 16:33:52' to Date
+			const parts = x.match(/(\d+) 年 (\d+) 月 (\d+) 日 (\d+):(\d+):(\d+)/);
+			if (!parts) return new Date();
+			const year = parseInt(parts[1]);
+			const month = parseInt(parts[2]) - 1; // Months are 0-indexed
+			const day = parseInt(parts[3]);
+			const hour = parseInt(parts[4]);
+			const minute = parseInt(parts[5]);
+			const second = parseInt(parts[6]);
+			return new Date(year, month, day, hour, minute, second);
+		}
+		).optional(),
 		heroImage: image().optional(),
 		tags: z.array(z.string()).optional(),
 	}),
