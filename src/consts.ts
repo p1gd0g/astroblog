c4f72ddffb46d67c0efed85bd383f147392c462e
renhao.szu <renhao.szu@gmail.com>
2025-09-25 07:41:12 +0000

    update

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'p1gd0g\'s blog';
-export const SITE_DESCRIPTION = '投资 旅游 编程 健身 一万年太短，不争朝夕';
+export const SITE_DESCRIPTION = 'Gridder 网格交易测试工具 - ATRX ETF 波动率排名';
