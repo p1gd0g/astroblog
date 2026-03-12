d88a1644911ce4c55d837c23910fb50d1a24a788
renhao.szu <renhao.szu@gmail.com>
2026-03-12 17:08:20 +0000

    add amp ads

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'p1gd0g\'s blog';
-export const SITE_DESCRIPTION = 'Gridder 网格交易测试工具 - ATRX ETF 波动率排名';
+export const SITE_DESCRIPTION = 'Gridder 网格交易测试工具 - ATRX ETF 波动率对比 - USDer - 美元/人民币理财对比';
